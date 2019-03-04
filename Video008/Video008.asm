03f0d1271a2cfab2b052f44847b216ec8ba41b4c
fraser125 <jkwall@hotmail.com>
2019-03-03 22:57:10 -0800

    Tweaked a few details.

--- Video008/Video008.asm
+++ Video008/Video008.asm
@@ -18,33 +18,18 @@ insert "../LIB/N64_BOOTCODE.BIN"
 Start:	                 // NOTE: base $80001000
 	init()
 
-	ScreenNTSC(320,200, BPP16, $A010'0000)
+	ScreenNTSC(320,240, BPP16, $A010'0000) // 153,600 = 0x2'5800
 
 	nop
 	nop
 	nop
 	
-	pixel8_init16($A010'0000, PAPAYA_WHIP16, LIGHT_STEEL_BLUE16) // yellow foreground, blue background
-	pixel8_init16($A011'0000, CRIMSON16, SILVER16) // yellow foreground, blue background
+	pixel8_init16($A010'1000, PAPAYA_WHIP16, LIGHT_STEEL_BLUE16) // 12,160 = 0x2F80 yellow foreground, blue background
+	pixel8_init16($A010'5000, CRIMSON16, SILVER16) // yellow foreground, blue background
 
-// Draw a Line (Horizontal)
-	// 20 rows from the top
-	// 100 Columns (110 + 100 + 110 = 320) 
-	// 200 Pixels tall
-	// 
-
-	lui t0, LIGHT_BLUE16
-	// ori t0, LIGHT_BLUE16
-	la t1, $A010'0000
-	
-	// 320 Pixels Wide		
-	addi t1, t1, ((320 * 20)  + 100) * 2	
-	addi t2, r0, 200
-do_Store2Pixels:
-	sw t0, 0x0(t1)
-	addi t2, t2, -1	
-	bne t2, r0, do_Store2Pixels
-	addi t1, t1, 320 * 2
+	nop
+	nop
+	nop
 	
 Loop:  // while(true);
 	j Loop
